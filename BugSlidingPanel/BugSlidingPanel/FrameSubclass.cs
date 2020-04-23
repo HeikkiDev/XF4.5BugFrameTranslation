bdc74f41ec2f1aea493261d60b1317ba0881eecb
Enrique Ramos Vargas <enrique.ramos@ibistic.com>
2020-04-23 13:56:06 +0200

    Repro project

---
+++ BugSlidingPanel/BugSlidingPanel/FrameSubclass.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace BugSlidingPanel
+{
+    public class FrameSubclass : Frame
+    {
+        private double _originalTranslationY;
+        private readonly PanGestureRecognizer _panGesture;
+
+        public FrameSubclass()
+        {
+            _panGesture = new PanGestureRecognizer();
+            _panGesture.PanUpdated += PanGesture_PanUpdated;
+            GestureRecognizers.Add(_panGesture);
+        }
+
+        ~FrameSubclass()
+        {
+            _panGesture.PanUpdated -= PanGesture_PanUpdated;
+        }
+
+        private void PanGesture_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    _originalTranslationY = TranslationY;
+                    break;
+                case GestureStatus.Running:
+                    TranslationY = _originalTranslationY + e.TotalY;
+                    break;
+                case GestureStatus.Canceled:
+                    _originalTranslationY = 0;
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Xamarin.Forms 4.5 bug: https://github.com/xamarin/Xamarin.Forms/issues/10422
